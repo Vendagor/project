1621db2fbc138849b0ef03302dde2813d3455faf
Alexander <mrunbeate6le20@gmail.com>
2021-04-01 23:31:56 +0300

    Switch

--- js/script.js
+++ js/script.js
@@ -23,3 +23,37 @@ const a = prompt('Один из последних просмотренных ф
    
 
 
+
+     /* if (4 == 4) {
+         console.log('Ok!');
+      } else {
+         console.log('Error!');
+      }
+
+
+
+if (num < 49) {
+   console.log('Error!');
+} else if (num > 100) {
+   console.log('Many');
+} else {
+   console.log('Ok!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error!');*/
+
+const num = 50;
+
+switch /* Строгое сравнение*/ (num) {
+   case 49:
+      console.log('None');
+      break;
+   case 100:
+      console.log('None');
+      break;
+   case 51: 
+      console.log('Ok');
+      break;
+   default:
+      console.log('Sorry');
+}
