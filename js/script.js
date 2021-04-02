ad89f2c3eb2f8b376b7464289cb7468394c5bd32
Alexander <mrunbeate6le20@gmail.com>
2021-04-02 16:26:42 +0300

    Loop and condition

--- js/script.js
+++ js/script.js
@@ -10,16 +10,60 @@ const personalMovieDB = {
    privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один из последних просмотренных фильмов?', ''),
+         b = prompt('На сколько оцените его?', '');
+      
+if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+
+      personalMovieDB.movies[a] = b;
+      console.log('Done');
+   } else {
+      console.log('error');
+      i--;
+   }
+}
+
+/*let i = 0;
+
+while (i < 2) {
+   const a = prompt('Один из последних просмотренных фильмов?', ''),
+         b = prompt('На сколько оцените его?', '');
+         i++;
+
+if (a != null && b != null && a!= '' && b != '' && a.length < 50) {
+
+      personalMovieDB.movies[a] = b;
+      console.log('Done');
+   } else {
+      console.log('error');
+      i--;
+   }
+}*/
+
+
+
+
+
+if (personalMovieDB.count < 10) {
+   console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+   console.log("Вы киноман");
+} else {
+   console.log("Произошла ошибка");
+}
+
+
+         
+
+console.log(personalMovieDB);
 
       
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
 
-      console.log(personalMovieDB);
+
+
    
 
 
@@ -42,9 +86,9 @@ if (num < 49) {
 
 (num === 50) ? console.log('Ok!') : console.log('Error!');*/
 
-const num = 50;
-
-switch /* Строгое сравнение*/ (num) {
+//const num = 50;
+/* Строгое сравнение*/
+/*switch (num) {
    case 49:
       console.log('None');
       break;
@@ -56,4 +100,25 @@ switch /* Строгое сравнение*/ (num) {
       break;
    default:
       console.log('Sorry');
-}
+}*/
+
+//let num = 50;
+
+//while (num <= 55) {
+   //console.log(num);
+   //num++;
+//}
+
+
+ //do {
+   //console.log(num);
+   //num++;
+ //}
+ //while (num <= 55);
+
+ //for (let i = 1; i < 10; i++) {
+    //if (i === 6) {
+      // continue;
+   // }
+   // console.log(i);
+ //}
